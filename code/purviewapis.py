5f3a7542d1847933e421bda30dabb3f1ac3375df
SKA <sha.anand@outlook.com>
2022-03-28 23:10:36 -0500

    Added Schedules

--- code/purviewapis.py
+++ code/purviewapis.py
@@ -5,47 +5,28 @@ import requests
 import os
 import csv
 
-url = 'https://login.microsoftonline.com/{replacetenantid}/oauth2/token'
+purviewinstancename='zzazurepurview01'
+tenantid='72f988bf-86f1-41af-91ab-2d7cd011db47'
+purviewdatasourcename='SqlServer-MoF'
+
+url = 'https://login.microsoftonline.com/'+ tenantid +'/oauth2/token'
 myobj = {'client_id': '','client_secret': '','grant_type': 'client_credentials','resource': 'https://purview.azure.net'}
 x = requests.post(url, data = myobj)
 x1=  x.json()
 
 #{'token_type': 'Bearer', 'expires_in': '86399', 'ext_expires_in': '86399', 'expires_on': '1648325241', 'not_before': '1648238541', 'resource': 'https://purview.azure.net', 'access_token': ''}
 #x1= json.dumps(x.text)
-#print(x1["token_type"])
+#print(x1["access_token"])
 
 mybearertoken = "Bearer " + x1["access_token"]
 
-#myheaders = {"Authorization": "Bearer MYREALLYLONGTOKENIGOT"}
 myheaders = {"Authorization":  mybearertoken }
 
-url = 'https://zzazurepurview01.scan.purview.azure.com/datasources?api-version=2018-12-01-preview'
+url = 'https://'+ purviewinstancename +'.scan.purview.azure.com/datasources?api-version=2018-12-01-preview'
 listdatasources = requests.get(url,headers=myheaders)
 #print(listdatasources.json())
 
 #https://zzazurepurview01.scan.purview.azure.com/datasources/SqlServer-MoF/scans/Scan-3aS?api-version=2018-12-01-preview
-
-mydata={
-    "properties": {
-        "credential": {
-            "referenceName": "cred",
-            "credentialType": "SqlAuth"
-        },
-        "serverEndpoint": "WINDEV2112EVAL\\\SK1",
-        "databaseName": "AdventureWorks2019z2",
-        "scanRulesetName": "SqlServer",
-        "scanRulesetType": "System",
-        "collection": {
-            "referenceName": "zzazurepurview01",
-            "type": "CollectionReference"
-        },
-            "connectedVia": {
-            "referenceName": "IntegrationRuntime-Quc",
-            "integrationRuntimeType": "SelfHosted"
-        }
-    },
-    "kind": "SqlServerDatabaseCredential"
-}
 
 infile_folder = "C:\\purview apis\\scanlist.txt"
 scanname=""
@@ -60,18 +41,23 @@ with open(infile_folder) as csv_file:
     for row in csv_reader:
         scanname = row[0]
         dbname = row[1]
+        recurrenceperiod=row[2]
+        scanhour=row[3]
+        scanminute=row[4]
+        scanday=row[5]
         print(f'\t{row[0]} | {row[1]} ')
         line_count += 1
 
         mydata='{    "properties": {        "credential": {            "referenceName": "cred",            "credentialType": "SqlAuth"        },        "recurrenceInterval": null,        "serverEndpoint": "WINDEV2112EVAL\\\SK1",        "databaseName": "'+ dbname +'",        "scanRulesetName": "SqlServer",        "scanRulesetType": "System",        "collection": {            "referenceName": "zzazurepurview01",            "type": "CollectionReference"        },            "connectedVia": {            "referenceName": "IntegrationRuntime-Quc",            "integrationRuntimeType": "SelfHosted"        }    },    "kind": "SqlServerDatabaseCredential"}'
         mydata1=json.loads(mydata)
-        url = 'https://zzazurepurview01.scan.purview.azure.com/datasources/SqlServer-MoF/scans/'+ scanname +'?api-version=2018-12-01-preview'
-        out=requests.put(url,json=mydata1,headers=myheaders)
-        print(out.json())
+        url = 'https://'+ purviewinstancename +'.scan.purview.azure.com/datasources/'+ purviewdatasourcename +'/scans/'+ scanname +'?api-version=2018-12-01-preview'
+        #out=requests.put(url,json=mydata1,headers=myheaders)
+        #print(out.json())
 
-        mydata1='{"properties": {"recurrence": {"frequency": "Month","interval": 1,"startTime": "2022-03-25T02:04:00Z", "endTime": null,"schedule": {"minutes": [0],"hours": [7],"weekDays": null,"monthDays": [2],"monthlyOccurrences": null},"timezone": "UTC"},"recurrenceInterval": null,"createdAt": "2022-03-25T02:06:02.5748139Z","lastModifiedAt": "2022-03-25T02:06:02.5748139Z","lastScheduled": null,"scanLevel": "Incremental" },"name": "default"}'
+        mydata='{  "properties": {    "recurrenceInterval": null,    "scanLevel": "Incremental",    "recurrence": {      "startTime": "2022-03-28T14:59:00.416Z",      "endTime": "2023-03-25T00:00:00.000Z",      "interval": 1,      "frequency": "Month",      "schedule": {        "hours": [          ' + scanhour +'        ],        "minutes": [          '+ scanminute +'        ],        "monthDays": [          '+ scanday +'        ]      }    }  }} '
         mydata1=json.loads(mydata)
-        url = 'https://zzazurepurview01.scan.purview.azure.com/datasources/SqlServer-MoF/scans/'+ scanname +'/triggers/default?api-version=2018-12-01-preview'
+        url = 'https://'+ purviewinstancename +'.scan.purview.azure.com/datasources/'+ purviewdatasourcename +'/scans/'+ scanname +'/triggers/default?api-version=2018-12-01-preview'
+        print(mydata1)
         out=requests.put(url,json=mydata1,headers=myheaders)
         print(out.json())
 
